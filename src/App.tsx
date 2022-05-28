45ba236840199f8c11415d08b5007494ec857218
Mednoob <ahmadyaqdhan123@gmail.com>
2022-05-28 13:03:04 +0900

    feat: use typescript and remove some CRA stuff

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <p className="font-bold text-2xl">Hello, World!</p>
+        </div>
+    )
+}
